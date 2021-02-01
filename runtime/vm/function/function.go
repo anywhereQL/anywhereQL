ad05093e36b71ad6cc307146e04e92c3fb53923b
KAWASAKI Yasukazu <yakawa.1128+github@gmail.com>
2021-02-02 08:23:44 +0900

    算術関数色々

--- runtime/vm/function/function.go
+++ runtime/vm/function/function.go
@@ -11,7 +11,23 @@ type CallFunction func([]value.Value) (value.Value, error)
 var funcs = map[string]CallFunction{
 	"abs": Abs,
 	"sign": Sign,
-
+	"sgn": Sign,
+	"floor": Floor,
+	"ceil": Ceil,
+	"ceiling": Ceil,
+	"round": Round,
+	"trunc": Trunc,
+	"truncate": Trunc,
+	"pow": Pow,
+	"power": Pow,
+	"sqrt": Sqrt,
+	"exp": Exp,
+	"ln": Ln,
+	"log10": Log10,
+	"sin": Sin,
+	"cos": Cos,
+	"cosh": Cosh,
+	"tan": Tan,
 }
 
 func LookupFunction(name string) CallFunction {
