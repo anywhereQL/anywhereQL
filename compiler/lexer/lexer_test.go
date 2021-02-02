52463979450409221c026afeb3159c6291139ad3
Yasukazu KAWASAKI <yakawa.1128+github@gmail.com>
2021-02-03 01:25:59 +0900

    テストケース

--- compiler/lexer/lexer_test.go
+++ compiler/lexer/lexer_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/anywhereQL/anywhereQL/common/token"
+	"github.com/anywhereQL/anywhereQL/common/value"
 )
 
 func TestLexer(t *testing.T) {
@@ -18,6 +19,109 @@ func TestLexer(t *testing.T) {
 				{
 					Type:    token.K_SELECT,
 					Literal: "SELECT",
+				},
+				{
+					Type: token.EOS,
+				},
+			},
+			isError: false,
+		},
+		{
+			input: "SELECT 1",
+			expected: token.Tokens{
+				{
+					Type:    token.K_SELECT,
+					Literal: "SELECT",
+				},
+				{
+					Type:    token.NUMBER,
+					Literal: "1",
+					Value: value.Value{
+						Type: value.INTEGER,
+						Int:  1,
+					},
+				},
+				{
+					Type: token.EOS,
+				},
+			},
+			isError: false,
+		},
+		{
+			input: "SELECT 1 + 2",
+			expected: token.Tokens{
+				{
+					Type:    token.K_SELECT,
+					Literal: "SELECT",
+				},
+				{
+					Type:    token.NUMBER,
+					Literal: "1",
+					Value: value.Value{
+						Type: value.INTEGER,
+						Int:  1,
+					},
+				},
+				{
+					Type:    token.S_PLUS,
+					Literal: "+",
+				},
+				{
+					Type:    token.NUMBER,
+					Literal: "2",
+					Value: value.Value{
+						Type: value.INTEGER,
+						Int:  2,
+					},
+				},
+				{
+					Type: token.EOS,
+				},
+			},
+			isError: false,
+		},
+		{
+			input: "SELECT ABS(1 + 2);",
+			expected: token.Tokens{
+				{
+					Type:    token.K_SELECT,
+					Literal: "SELECT",
+				},
+				{
+					Type:    token.IDENT,
+					Literal: "ABS",
+				},
+				{
+					Type:    token.S_LPAREN,
+					Literal: "(",
+				},
+				{
+					Type:    token.NUMBER,
+					Literal: "1",
+					Value: value.Value{
+						Type: value.INTEGER,
+						Int:  1,
+					},
+				},
+				{
+					Type:    token.S_PLUS,
+					Literal: "+",
+				},
+				{
+					Type:    token.NUMBER,
+					Literal: "2",
+					Value: value.Value{
+						Type: value.INTEGER,
+						Int:  2,
+					},
+				},
+				{
+					Type:    token.S_RPAREN,
+					Literal: ")",
+				},
+				{
+					Type:    token.S_SEMICOLON,
+					Literal: ";",
 				},
 				{
 					Type: token.EOS,
