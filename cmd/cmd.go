00caf1ba5731185b5a1431452322cea032e38d3b
Yasukazu KAWASAKI <yakawa.1128+github@gmail.com>
2021-02-13 07:27:25 +0900

    Version

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -25,10 +25,10 @@ var (
 	}
 	versionCmd = &cobra.Command{
 		Use:   "version",
-		Short: "Print the version number of Hugo",
-		Long:  `All software has versions. This is Hugo's`,
+		Short: "Print the version number of anywhereQL",
+		Long:  `All software has versions. This is anywhereQL's`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+			fmt.Println("v0.0.2")
 		},
 	}
 )
